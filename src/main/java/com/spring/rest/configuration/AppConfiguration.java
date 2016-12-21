bd4cf0f0d91ff97a0e7a3fcf377ae57fb5d6f81b
Rishikesh Shukla <rishikesh.shukla@gslab.com>
2016-12-21 15:17:48 +0530

    adding source files

---
+++ src/main/java/com/spring/rest/configuration/AppConfiguration.java
@@ -0,0 +1,13 @@
+package com.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.spring.rest")
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/com/spring/rest/configuration/AppInitializer.java
@@ -0,0 +1,33 @@
+package com.spring.rest.configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(AppConfiguration.class);
+
+		/*
+		 * javax.servlet.FilterRegistration.Dynamic corsFilter =
+		 * container.addFilter("simpleCORSFilter", SimpleCORSFilter.class);
+		 * corsFilter.addMappingForUrlPatterns(null, true, "/*");
+		 */
+
+		container.addListener(new ContextLoaderListener(appContext));
+		appContext.setServletContext(container);
+
+		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(appContext));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/*");
+
+	}
+
+}
